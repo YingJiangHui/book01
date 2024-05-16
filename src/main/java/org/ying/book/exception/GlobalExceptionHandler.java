27dab436e6f3173c50fbc4afdb8d3645933e5bf8
Jh <473380917@qq.com>
2024-05-16 11:11:07 +0800

    fix: 邮箱发送失败问题

--- src/main/java/org/ying/book/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ying/book/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package org.ying.book.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.SendFailedException;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.angus.mail.smtp.SMTPAddressFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSendException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -31,6 +35,18 @@ public class GlobalExceptionHandler {
         Result result = Result.builder().code(HttpStatus.NOT_FOUND.value()).message(ex.toString()).build();
         return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
 
+    }
+
+    @ExceptionHandler(MailSendException.class)
+    public ResponseEntity<Result> handleException(MailSendException ex) {
+        Result result = Result.builder().code(HttpStatus.UNPROCESSABLE_ENTITY.value()).message("电子邮件格式不正确，请检查").build();
+        return new ResponseEntity<>(result, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<Result> handleException(MessagingException ex) {
+        Result result = Result.builder().code(HttpStatus.UNPROCESSABLE_ENTITY.value()).message("电子邮件发送失败，请检查").build();
+        return new ResponseEntity<>(result, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(Exception.class)
