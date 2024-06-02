7fe317ec76efb2505d0ade06250fe3c72b2198f5
Jh <473380917@qq.com>
2024-06-02 17:46:54 +0800

    feat: 创建search 表以及相关操作

---
+++ src/main/java/org/ying/book/mapper/SearchMapper.java
@@ -0,0 +1,33 @@
+package org.ying.book.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+import org.ying.book.pojo.Search;
+import org.ying.book.pojo.SearchExample;
+
+public interface SearchMapper {
+    long countByExample(SearchExample example);
+
+    int deleteByExample(SearchExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Search row);
+
+    int insertSelective(Search row);
+
+    List<Search> selectByExampleWithRowbounds(SearchExample example, RowBounds rowBounds);
+
+    List<Search> selectByExample(SearchExample example);
+
+    Search selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("row") Search row, @Param("example") SearchExample example);
+
+    int updateByExample(@Param("row") Search row, @Param("example") SearchExample example);
+
+    int updateByPrimaryKeySelective(Search row);
+
+    int updateByPrimaryKey(Search row);
+}
---
+++ src/main/java/org/ying/book/pojo/Search.java
@@ -0,0 +1,75 @@
+package org.ying.book.pojo;
+
+import java.util.Date;
+
+public class Search {
+    private Integer id;
+
+    private Integer userId;
+
+    private String email;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private Boolean deleted;
+
+    private String target;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+}
